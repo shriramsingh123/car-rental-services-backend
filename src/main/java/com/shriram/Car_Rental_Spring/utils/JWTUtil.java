063d03d4eb5727a568610cc68bbfa245ebed0898
Shri Ram Kumar <135927276+shriramsingh123@users.noreply.github.com>
2024-03-15 12:41:06 +0530

    Update JWTUtil.java

--- src/main/java/com/shriram/Car_Rental_Spring/utils/JWTUtil.java
+++ src/main/java/com/shriram/Car_Rental_Spring/utils/JWTUtil.java
@@ -16,61 +16,7 @@ import java.util.function.Function;
 
 @Component
 public class JWTUtil {
-/*
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
-
-    public String extractUserName(String token){
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    public String generateToken(UserDetails userDetails){
-        return generateToken(new HashMap<>(),userDetails);
-    }
-
-    public boolean isTokenValid(String token, UserDetails userDetails){
-        final String userName = extractUserName(token);
-        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
-
-    private <T> T extractClaim(String token, Function<Claims,T> claimsResolvers){
-        final Claims claims = extractAllClaims(token);
-        return claimsResolvers.apply(claims);
-    }
-
-    private String generateToken(Map<String,Object> extraClaims,UserDetails userDetails){
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*24))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
-    }
-
-    public String generateRefreshToken(Map<String,Object> extraClaims , UserDetails userDetails){
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+604800000))
-                .signWith(getSigningKey(),SignatureAlgorithm.HS256).compact();
-    }
-
-    private boolean isTokenExpired(String token){
-        return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token){
-        return extractClaim(token,Claims::getExpiration);
-    }
-
-    private Claims extractAllClaims(String token){
-        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token)
-                .getBody();
-    }
-
-    private Key getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-*/
-
-
+    
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
     public String extractUsername(String token) {
